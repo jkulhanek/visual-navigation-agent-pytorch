e3eddb16fb43ad2c0f49fdbd2aeeda6f14aca254
Jonas Kulhanek <jonas.kulhanek@live.com>
2018-10-02 14:08:01 +0200

    Fix restore flag on train

--- bin/train.py
+++ bin/train.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     device = torch.device('cpu')
 
-    if args.restore:
+    if args['restore']:
         t = Training.load_checkpoint(args)
     else:
         t = Training(device, args)
